e6d66df3e0e5c109961503d4b34bb173308411d5
unknown <saltycds@gmail.com>
2026-01-08 13:39:04 +0100

    -

--- Track Studio/bin/Release/net5.0/Lib/AINB/json2ainb.py
+++
@@ -1,8 +0,0 @@
-import json
-import ainb
-
-with open("in", 'r', encoding='utf-8') as file:
-    data = json.load(file)
-file = ainb.AINB(data, from_dict=True)
-with open("out", 'wb') as outfile:
-    file.ToBytes(file, outfile)
